05ba9aa8a2dd1a3da39db0f32816925a68e50456
cpriya <cpriya@DIN76000004.corp.capgemini.com>
2019-07-04 11:34:02 +0530

    My first Commit

---
+++ MyFirstProject/src/main/java/org/cap/demo/Sample.java
@@ -0,0 +1,16 @@
+package org.cap.demo;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("Haii");
+        SubDemo sub=new SubDemo();
+        sub.show();
+        display();
+	}
+	
+	public static void display() {
+		System.out.println("Display method");
+	}
+
+}
---
+++ MyFirstProject/src/main/java/org/cap/demo/SubDemo.java
@@ -0,0 +1,11 @@
+package org.cap.demo;
+
+public class SubDemo {
+	public void show() {
+		for(int i=0;i<=5;i++) {
+			System.out.println(i);
+		}
+	}
+	
+
+}
